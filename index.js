9ec2b8c63d263dfbbcb33d914c2da673d77645ed
Zom <skyholy6@yahoo.com.tw>
2018-08-01 09:42:09 +0800

    7/31 modify

--- index.js
+++ index.js
@@ -19,14 +19,35 @@ app.use(function (err, req, res, next) {
 });
 */
 
+app.set('port', config.port);
+var cluster = require('cluster');
+var http = require('http');
+var numCPUs = process.argv[2] || require('os').cpus().length;
+//numCPUs = 1;
+if (cluster.isMaster) {
+        for (var i = 0; i < numCPUs; i++) {
+                cluster.fork();
+        }
+
+        cluster.on('listening', (worker, address) => {
+                //console.log('worker ' + worker.process.pid +', listen: '+address.address+":"+address.port);
+        });
+        cluster.on('exit', (worker, code, signal) => {
+                //console.log('worker ' + worker.process.pid + ' died');
+                cluster.fork();
+        });
+} else {
+        app.listen(app.get('port'));
+}
+
 app.use(function (err, req, res, next) {
 	//console.error(err.stack)
 	res.status(500).send({"error":"error parameter","status":"1","message":err.stack})
 })
 
-app.set('port', config.port);
+//app.set('port', config.port);
 
-
+/*
 if (module.parent) {
   module.exports = app;
 } else {
@@ -34,4 +55,4 @@ if (module.parent) {
     console.log(`listening on port ${config.port}`);
   });
 }
-
+*/
